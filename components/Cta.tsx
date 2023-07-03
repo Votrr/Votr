dd86e390f383755462d7fa078f2f9d0529a9ff32
David-glitc <davidopuene8@gmail.com>
2023-07-03 08:33:52 +0100

    First commit

---
+++ components/Cta.tsx
@@ -0,0 +1,12 @@
+
+function Cta() {
+  return (
+    <div>
+      <button className=" px-6 py-2 text-gray-800 rounded-xl bg-gray-300 shadow-lg">
+        Try votr↗
+      </button>
+    </div>
+  );
+}
+
+export default Cta;
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,71 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { DocsThemeConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import Cta from "./components/Cta";
 
 const config: DocsThemeConfig = {
-  logo: <span className=" font-bold text-2xl shadow-lg shadow-blue-600">Votr.io</span>,
+  logo: (
+    <span className="logo text-2xl font-semibold shadow-md shadow-sky-800 font-sans text-gray-500">
+      Votr.io
+    </span>
+  ),
   project: {
-    link: 'https://github.com/David-glitc/Docs.Votr_io',
+    link: "https://github.com/david-glitc/Docs.Votr_io",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/David-glitc/Docs.Votr_i',
+  docsRepositoryBase: "https://github.com/david-glitc/Docs.Votr_io",
   footer: {
-    text: 'Votr.io',
+    text: (
+      <span>
+        {new Date().getFullYear()} ©{" "}
+        <a href="https://nextra.site" target="_blank">
+          Votr.io
+        </a>
+        .
+      </span>
+    ),
   },
-}
 
-export default config
+  sidebar: {
+    autoCollapse: true,
+    toggleButton: true,
+  },
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath !== "/") {
+      return {
+        titleTemplate: "%s – Votr.io",
+      };
+    }
+  },
+  navbar: {
+    extraContent: Cta
+  },
+  head: (
+    <Head>
+      <title>
+        Votr.io | Revolutionizing Voting with the Blockchain
+      </title>
+      <meta
+        name="description"
+        content="Votr.io is an innovative platform that leverages blockchain technology to revolutionize electronic voting. Join us and experience a new era of secure and transparent voting."
+      />
+      <meta
+        property="og:title"
+        content="Votr.io | Revolutionizing Electronic Voting through Blockchain"
+      />
+      <meta
+        property="og:description"
+        content="Votr.io is an innovative platform that leverages blockchain technology to revolutionize electronic voting. Join us and experience a new era of secure and transparent voting."
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://www.votr-io.vercel.app" />
+      <meta property="og:image" content="/images/votr-io-logo.png" />
+      <meta name="twitter:card" content="summary_large_image" />
+    </Head>
+  ),
+};
+
+export default config;
